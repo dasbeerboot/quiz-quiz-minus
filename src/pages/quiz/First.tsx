586c6d7301adde5f9f4d4472cb443fa4587dad6c
Juwon Chun <juwonchun@gmail.com>
2021-06-16 23:21:34 +0900

    temporary commit

--- src/pages/quiz/First.tsx
+++ src/pages/quiz/First.tsx
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import config from '../../configs/config'
 
+export interface FirstIProps {
+  question: string
+  answer: string[]
+}
 function First(): JSX.Element {
+  const [qna, setQna] = useState<FirstIProps>()
   const getQuiz = () => {
     axios({
       method: 'get',
       url: config.url,
     })
       .then((response) => {
-        const data = response.data.results[0]
-        console.log(data)
-        console.log('question', data.question)
-        console.log('choices', data.correct_answer, data.incorrect_answers)
+        // const data = response.data.results[0]
+        // setQna((prev) => ({...prev, question: data.question}))
+        // answerList = data.incorrect_answers
+        // answerList.push(data.correct_answer)
       })
       .catch((err) => {
         console.log(err)
       })
   }
+
   return (
     <article>
       first quiz
       <button onClick={getQuiz}>get quiz</button>
+      {/* <div>{question}</div> */}
+      <div>
+        <ul>
+          {/* {answerList.map((answer) => (<li>{answer}</li>))} */}
+        </ul>
+      </div>
     </article>
   )
 }
