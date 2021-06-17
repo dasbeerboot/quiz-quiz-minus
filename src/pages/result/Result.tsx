aa46edb3858962bfe0061714432eee5a0093a845
Juwon Chun <juwonchun@gmail.com>
2021-06-18 00:27:44 +0900

    publish result page

--- src/pages/result/Result.tsx
+++ src/pages/result/Result.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import moment from 'moment'
 import './Result.scss'
+import Button from '../../components/Button'
+import { useHistory } from 'react-router-dom'
 
 interface ResultPageIProps {
   result: boolean[]
@@ -8,6 +10,7 @@ interface ResultPageIProps {
 }
 
 function ResultPage({ result,startTime }: ResultPageIProps): JSX.Element {
+  const history = useHistory()
   const currentTime = moment()
   const duration = moment.duration(currentTime.diff(startTime)).asMinutes().toFixed(0)
 
@@ -18,18 +21,23 @@ function ResultPage({ result,startTime }: ResultPageIProps): JSX.Element {
         수고하셨습니다!
       </div>
       <div className="score">
-        당신의 점수는 <span>{numberOfCorrectAnswers * 20}</span> ({numberOfCorrectAnswers}/5) 점이며
+        당신의 점수는 <span className="value">{numberOfCorrectAnswers * 20}</span>({numberOfCorrectAnswers}/5) 점이며
       </div>
       <div className="duration">
-        총 소요시간은 <span>{duration}</span>분, 
+        총 소요시간은 <span className="value">{duration}</span>분, 
       </div>
-      <div className="answers">맞은 문제는  {numberOfCorrectAnswers}개, 틀린 문제는 {5 - numberOfCorrectAnswers}개 입니다.</div>
+      <div className="answers">맞은 문제는  
+      <span className="value">
+      {numberOfCorrectAnswers}</span>개, 틀린 문제는 <span className="value">{5 - numberOfCorrectAnswers}</span>개 입니다.</div>
+      
+
       {
         numberOfCorrectAnswers === 5 && 
         <div className="show-me-the-money">
           당신의 손에 쥐어지는 합격목걸이
         </div>
       }
+      <Button onClick={() => history.push('/')}>재도전!</Button>
     </article>
   )
 }
