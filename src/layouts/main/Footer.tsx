27d02f58b37d03c29838363c9ff05bf07393963e
Juwon Chun <juwonchun@gmail.com>
2021-06-18 00:07:11 +0900

    fix typo

--- src/layouts/main/Footer.tsx
+++ src/layouts/main/Footer.tsx
@@ -4,7 +4,7 @@ function Footer(): JSX.Element {
   return <footer className="quiz-footer">
     <div className="info">
       <p className="name">Juwon Chun</p>
-      <a className="url" href="github.io/dasbeerboot">github.io/dasbeerboot</a>
+      <a className="url" href="github.io/dasbeerboot">github.com/dasbeerboot</a>
     </div>
   </footer>
 }
