834b12e4eacd6778e987d03257901cb2f43de52e
Juwon Chun <juwonchun@gmail.com>
2021-06-18 00:32:53 +0900

    modify footer color

--- src/layouts/main/Footer.tsx
+++ src/layouts/main/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Footer.scss'
 
 function Footer(): JSX.Element {
-  return <footer className="quiz-footer">
+  return (<footer className="quiz-footer">
     <div className="info">
       <p className="name">Juwon Chun</p>
       <a className="url" href="github.io/dasbeerboot">github.com/dasbeerboot</a>
     </div>
-  </footer>
+  </footer>)
 }
 
 export default Footer
